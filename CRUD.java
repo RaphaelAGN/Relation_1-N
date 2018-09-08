af7f47cfa722e529b3820ea4d048655a02db2e24
Raphael <42879707+RaphaelAGN@users.noreply.github.com>
2018-09-08 13:07:54 -0300

    CRUD

---
+++ CRUD.java
@@ -0,0 +1,264 @@
+
+import java.util.Scanner;
+import java.io.*;
+
+//Classe View que contem os menus
+public class CRUD{
+   public static void main(String[] args) throws IOException {
+      Controle ct = new Controle("filmes.db");
+      Controle ctg = new Controle("generos.db");
+   
+      Scanner s = new Scanner(System.in);
+      
+      //menu de generos
+      int escolha;
+      String menu1 = "\nMenu de opcoes de generos: \n0. Ir para o menu de filmes. \n1. Adicionar Genero\n2. Alterar Genero\n3. Excluir Genero\n4. Busca\n";
+      
+      System.out.println(menu1);
+      System.out.print("O que deseja fazer: ");
+      escolha = s.nextInt();
+      
+      while(escolha!=0){
+         
+         switch(escolha){
+            case 1:
+               adicionarGenero(ctg);
+               break;
+            
+            case 2:
+               //alterarGenero(ct);
+               break;
+            
+            case 3:
+               //excluirGenero(ct);
+               break;
+            
+            case 4:
+               buscarGenero(ctg);
+               break;
+            
+            default:
+               System.out.print("\nNenhuma das opcoes foi escolhida. Digite apenas numeros de 0 a 4.\n");
+         }
+         
+         System.out.println(menu1);
+         System.out.print("O que deseja fazer: ");
+         escolha = s.nextInt();
+      }//fim while generos
+      
+      //menu de filmes
+      String menu2 = "\nMenu de opcoes de filmes: \n0. Encerrar o processo. \n1. Adicionar Filme\n2. Alterar Filme\n3. Excluir Filme\n4. Busca\n";
+         
+      System.out.println(menu2);
+      System.out.print("O que deseja fazer: ");
+      escolha = s.nextInt();
+
+      while(escolha!=0){
+         
+         switch(escolha){
+            case 1:
+               adicionar(ct);
+               break;
+            
+            case 2:
+               alterar(ct);
+               break;
+            
+            case 3:
+               excluir(ct);
+               break;
+            
+            case 4:
+               buscar(ct);
+               break;
+                  
+            default:
+               System.out.print("\nNenhuma das opcoes foi escolhida. Digite apenas numeros de 0 a 4.\n");
+         }
+         
+         System.out.println(menu2);
+         System.out.print("O que deseja fazer: ");
+         escolha = s.nextInt();
+      }//fim while
+      
+      System.out.print("\nProcesso encerrado.");
+   }//end main
+
+   /**
+    * Metodo de exclusao
+   **/
+   public static void excluir(Controle ct){
+      Scanner s = new Scanner(System.in);
+      int id;
+   
+      System.out.print("\nDigite a ID do filme a ser excluido: ");
+      id = s.nextInt();
+   
+      long pos = ct.busca(id);//utiliza o metodo de busca para pegar a posicao do filme a ser buscado caso o mesmo exista
+   
+      if(pos == -1)
+         System.out.println("\nRegistro nao encontrado.");
+      else{
+         System.out.print("\nTem certeza que deseja excluir?[s/n]: ");
+         switch(s.next().charAt(0)){
+            case 's': case 'S':
+               ct.excluir(pos);
+               System.out.println("\nRegistro excluido com sucesso.");
+               break;
+            case 'n': case 'N':
+               System.out.println("\nExclusao cancelada.");
+         }
+      }
+   }//end excluir
+   
+   
+   /**
+    * Metodo de adicao de filmes
+   **/
+   public static void adicionar(Controle ct) throws IOException {
+      Scanner s = new Scanner(System.in);
+      System.out.print("\nDigite as informacoes do filme a ser adicionado.\n");
+   
+      System.out.print("\nTitulo: ");
+      Filme f = new Filme(s.nextLine());
+   
+      System.out.print("\nTitulo Original: ");
+      f.setTituloOri(s.nextLine());
+   
+      System.out.print("\nPais de Origem: ");
+      f.setPais(s.nextLine());
+      
+      System.out.print("\nAno de lancamento: ");
+      f.setAno(s.nextShort());
+   
+      System.out.print("\nDuracao em minutos: ");
+      f.setDuracao(s.nextShort());
+      s.nextLine();
+   
+      System.out.print("\nDiretor: ");
+      f.setDiretor(s.nextLine());
+   
+      System.out.print("\nSinopse: ");
+      f.setSinopse(s.nextLine());
+      
+      System.out.print("\nID do genero(utilize numeros): ");
+      f.setIDGenero(s.nextInt());
+   
+      System.out.print("\nTem certeza que deseja adicionar o filme?[s/n]: ");
+      switch(s.next().charAt(0)){
+         case 's': case 'S':
+            f.setIDFilme(ct.novoID());
+            byte[] vet = f.getByteArray();
+            ct.vaiPFim();
+            ct.incluir(' ', vet);
+            System.out.println("\nFilme adicionado com sucesso!\n");
+            break;
+         case 'n': case 'N':
+            System.out.println("\nAdicao cancelada.\n");
+      
+      }
+   }//end adicionar
+   
+   public static void adicionarGenero(Controle ctg) throws IOException {
+      Scanner s = new Scanner(System.in);
+      System.out.print("\nDigite as informacoes do genero ser adicionado.\n");
+   
+      System.out.print("\nNome do genero: ");
+      Genero g = new Genero(s.nextLine());
+   
+      System.out.print("\nTem certeza que deseja adicionar o genero?[s/n]: ");
+      switch(s.next().charAt(0)){
+         case 's': case 'S':
+            g.setIDGenero(ctg.novoID());
+            byte[] vet = g.getByteArray();
+            ctg.vaiPFim();
+            ctg.incluir(' ', vet);
+            System.out.println("\nGenero adicionado com sucesso!\n");
+            break;
+         case 'n': case 'N':
+            System.out.println("\nAdicao cancelada.\n");
+      
+      }
+   }//end adicionar
+   
+   /**
+    * Metodo de busca para exclusao e pesquisa de generos
+    * @param instancia da classe controleGeneros
+   **/
+   public static void buscarGenero(Controle ctg){
+      Scanner s = new Scanner(System.in);
+      System.out.print("\nDigite a id, do genero, a ser buscada: ");
+      int id = s.nextInt();
+      ctg.mostrarGenero(id);
+   }//end buscar
+   
+   /**
+    * Metodo de busca para exclusao e pesquisa de filmes
+   **/
+   public static void buscar(Controle ct){
+      Scanner s = new Scanner(System.in);
+      System.out.print("\nDigite a id, do filme, a ser buscada: ");
+      int id = s.nextInt();
+      ct.mostrarFilme(id);
+   }//end buscar
+
+   /**
+    * Metodo de alteracao
+   **/
+   public static void alterar(Controle ct){
+      Scanner s = new Scanner(System.in);
+      int id;
+      System.out.print("\nDigite a ID do filme a ser alterado: ");
+      id = s.nextInt();
+      long pos = ct.busca(id);
+      Filme f = new Filme();
+      if(pos == -1)
+         System.out.println("\nRegistro nao encontrado.");
+      else{
+         System.out.print("\nTem certeza que deseja alterar? (A alteracao resultara na exclusao dos dados atuais) [s/n]:  ");
+         switch(s.next().charAt(0)){
+            case 's': case 'S':
+               
+               ct.excluir(pos);
+               System.out.println("\nRegistro excluido com sucesso");
+               try{
+                  s.nextLine();
+                  System.out.print("\nDigite as informacoes do filme a ser adicionado.\n");
+               
+                  System.out.print("\nTitulo: ");
+                  f.setTitulo(s.nextLine());
+               
+                  System.out.print("\nTitulo Original: ");
+                  f.setTituloOri(s.nextLine());
+                  System.out.print("\nPais de Origem: ");
+                  f.setPais(s.nextLine());
+               
+                  System.out.print("\nAno de lancamento: ");
+                  f.setAno(s.nextShort());
+               
+                  System.out.print("\nDuracao em minutos: ");
+                  f.setDuracao(s.nextShort());
+                  s.nextLine();
+               
+                  System.out.print("\nDiretor: ");
+                  f.setDiretor(s.nextLine());
+               
+                  System.out.print("\nSinopse: ");
+                  f.setSinopse(s.nextLine());
+                  
+                  f.setIDFilme(id);
+                  byte[] vet = f.getByteArray();
+                  ct.vaiPFim();
+                  ct.incluir(' ', vet);
+                  System.out.println("\nFilme alterado com sucesso!\n");
+                        
+               }catch(IOException E){
+                  E.printStackTrace();
+               }
+               break;
+            case 'n': case 'N':
+               System.out.println("\nAlteracao cancelada.");
+         }
+      }
+   }//end alterar
+}//end class
